0b74f241579332b6321e8c6e7b1df0ebba66a180
Samuel Dias Oliveira <samueldias004@gmail.com>
2025-12-03 19:30:19 -0300

    aoc-day-2-part-2

--- ac2025/src/main/java/org/example/daytwo/DayTwoPartTwo.java
+++ ac2025/src/main/java/org/example/daytwo/DayTwoPartTwo.java
@@ -5,15 +5,16 @@ public class DayTwoPartTwo {
     public static void main(String[] args) {
 
         /*
-        * Pega os ranges, itera entre eles
-        * Pega cada valor, da split na metade e testa se ambas as partes são iguais
-        * Pega cada valor, da split em partes iguais e testa se todas as partes são iguais
-        * Se forem iguais, soma no contador
-        * */
+         * Pega os ranges, itera entre eles
+         * Pega cada valor, da split na metade e testa se ambas as partes são iguais
+         * Pega cada valor, da split em partes iguais e testa se todas as partes são iguais
+         * Se forem iguais, soma no contador
+         * */
         String inputReal = "1061119-1154492,3-23,5180469-5306947,21571-38630,1054-2693,141-277,2818561476-2818661701,21177468-21246892,40-114,782642-950030,376322779-376410708,9936250-10074071,761705028-761825622,77648376-77727819,2954-10213,49589608-49781516,9797966713-9797988709,4353854-4515174,3794829-3861584,7709002-7854055,7877419320-7877566799,953065-1022091,104188-122245,25-39,125490-144195,931903328-931946237,341512-578341,262197-334859,39518-96428,653264-676258,304-842,167882-252124,11748-19561";
         String inputTest = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
         // Expected Output Test: 4174379265
-        String[] intervals = inputTest.split(",");
+
+        String[] intervals = inputReal.split(",");
         DayTwoPartTwo dayTwo = new DayTwoPartTwo();
 
         for (String interval : intervals) {
@@ -41,29 +42,64 @@ public class DayTwoPartTwo {
         int lengthString = stringfiedId.length();
 
         if (lengthString % 2 == 0) {
-
             String firstPart = stringfiedId.substring(0, (lengthString / 2));
             String secondPart = stringfiedId.substring((lengthString / 2));
 
             if (firstPart.equals(secondPart)) {
                 this.counter += id;
                 System.out.println("Invalid: " + id);
-            }
-        }
+            } else {
+                if (id < 100) return;
+                String[] splitedArr = new String[lengthString / 2];
+                int index = 0;
+                for (int i = 0; i <= lengthString; i++) {
+                    if (i % 2 == 0 && i != 0) {
+                        if (i == 2) {
+                            splitedArr[index] = stringfiedId.substring(0, i);
+                            index++;
+                            continue;
+                        }
+                        splitedArr[index] = stringfiedId.substring(i - 2, i);
+                        index++;
+                    }
+                }
+                int allEqual = 0;
+                for (int i = 0; i < splitedArr.length; i++) {
+                    if (splitedArr[0].equals(splitedArr[i])) {
+                        allEqual++;
+                        continue;
+                    }
+                }
 
-        if (lengthString % 2 == 1) {
+                if (allEqual == splitedArr.length) {
+                    this.counter += id;
+                    System.out.println("Invalid: " + id);
+                }
+            }
+        } else if (lengthString % 3 == 0) {
 
             String firstPart = stringfiedId.substring(0, (lengthString / 3));
             String secondPart = stringfiedId.substring((lengthString / 3), ((lengthString / 3) * 2));
             String thirdPart = stringfiedId.substring(((lengthString / 3) * 2), ((lengthString / 3) * 3));
 
             if (firstPart.equals(secondPart) && secondPart.equals(thirdPart)) {
-                System.out.println("Invalid: " + id);
-//                System.out.println(firstPart + " " + secondPart + " " + thirdPart);
+                System.out.println("Invalid impar: " + id);
                 this.counter += id;
             }
+        } else {
+            if (id < 10) return;
+            String[] splitedArr = id.toString().split("");
+            int countEqualLetter = 0;
+            for (String str : splitedArr) {
+                if (str.equals(splitedArr[0])) {
+                    countEqualLetter++;
+                }
+            }
+            if (countEqualLetter == splitedArr.length) {
+                this.counter += id;
+                System.out.println("Invalid all equal: " + id);
+            }
         }
-
     }
 
     void getResult() {
